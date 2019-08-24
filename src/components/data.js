f16dd50ca1bcf850b6972a8ee8c8d2789ce5d79f
Soroush <soroush.ecu@yahoo.com>
2019-08-24 11:27:03 -0700

    reactUpdated

--- src/components/data.js
+++ src/components/data.js
@@ -6,73 +6,90 @@ class Data extends Component {
 constructor(props) {
     super(props);
     this.state = {
-        options: {
-            chart: {
-                background: '#f4f4f4',
-                    foreColor: '#333'
-            },
-            xaxis: {
-                categories: 
-									newTimesArray,
-                  title: {
-                    text: 'Time',
-                    style:{
-                      fontSize:"25px"
-                    }
-                }
-                
-            },
-            plotOptions: {
-                bar: {
-                  horizontal: false
-                }
-            },
-            fill: {
-              colors: ["#F44336"]
-            },
-            dataLabels: {
-              enabled: true
-            },
-          
-            title: {
-              text: "Time and Frequency",
-              text2: "Time and Speed",
-              align: "center",
-              margin: 20,
-              offsetY: 20,
-              padding:1000,
-              style: {
-                fontSize: "25px"
-              }
-            },
-            title2: {
-              text: "Time and Speed",
-              align: "center",
-              margin: 20,
-              offsetY: 30,
-              style: {
-                fontSize: "25px"
-              }
-            }
-        },
-        series: [{
-                name: 'Frequency',
-                data: newFrequencyArray,
-                title: {
-                  text: 'Time',
-                  style:{
-                    fontSize:"25px"
-                  }
-              }
-        }],
-        firstSpeed: [{name: 'Speed', data: newSpeedArray }],
-        secondFrequency: [{name: 'Frequency', data: secondFrequency}],
-        secondSpeed: [{name: 'Speed', data: secondSpeed}],
-        thirdFrequency: [{name: 'Frequency', data: thirdFrequency}],
-        thirdSpeed: [{name: 'Speed', data: thirdSpeed}]
-        
+        options:{}
       
-    }
+		}
+		
+}
+
+componentWillMount(){
+	this.options();
+}
+
+options() {
+this.setState({
+	options: {
+		chart: {
+				background: '#f4f4f4',
+						foreColor: '#333'
+		},
+		xaxis: {
+				categories: 
+					newTimesArray,
+					title: {
+						text: 'Time',
+						style:{
+							fontSize:"25px"
+						}
+				}
+				
+		},
+		plotOptions: {
+				bar: {
+					horizontal: false
+				}
+		},
+		fill: {
+			colors: ["#F44336"]
+		},
+		dataLabels: {
+			enabled: true
+		},
+	
+		title: {
+			text: "Time and Frequency",
+			// text2: "Time and Speed",
+			align: "center",
+			margin: 20,
+			offsetY: 20,
+			style: {
+				fontSize: "25px"
+			}
+		},
+		// title2: {
+		//   text: "Time and Speed",
+		//   align: "center",
+		//   margin: 20,
+		//   offsetY: 30,
+		//   style: {
+		//     fontSize: "25px"
+		//   }
+		// }
+},
+series: [{
+				name: 'Frequency',
+				data: newFrequencyArray,
+				title: {
+					text: 'Time',
+					style:{
+						fontSize:"25px"
+					}
+			}
+}],
+
+
+firstSpeed: [{name: 'Speed', data: newSpeedArray }],
+secondFrequency: [{name: 'Frequency', data: secondFrequency}],
+secondSpeed: [{name: 'Speed', data: secondSpeed}],
+thirdFrequency: [{name: 'Frequency', data: thirdFrequency}],
+thirdSpeed: [{name: 'Speed', data: thirdSpeed}]
+
+
+
+
+
+})
+
 }
 
 render() {
